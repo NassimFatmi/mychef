acc97837e471a8d18f4ead17f8623f4ab8282398
AkliYalaoui <akliyalaoui16@gmail.com>
2021-03-15 14:13:46 +0100

    added laravel controllers and models

---
+++ API/database/migrations/2021_03_15_125917_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('chef_id');
+            $table->unsignedBigInteger('user_id');
+
+            $table->foreign('chef_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+
+            $table->time("deadline");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
