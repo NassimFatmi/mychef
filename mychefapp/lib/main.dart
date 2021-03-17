3f0a1325be9678391e18c0f3c9e44b8afd6b04ae
NassimFatmi <in_fatmi@esi.dz>
2021-03-17 16:32:07 +0100

    update services

--- mychefapp/lib/main.dart
+++ mychefapp/lib/main.dart
@@ -48,7 +48,7 @@ class MyApp extends StatelessWidget {
                   : Colors.black),
         ),
       ),
-      home: HomeScreen(),
+      home: AuthScreen(),
     );
   }
 }
--- mychefapp/lib/screens/home_screen.dart
+++ mychefapp/lib/screens/home_screen.dart
@@ -9,19 +9,31 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          children: [
-            Icon(
-              Icons.restaurant_menu,
-            ),
-            SizedBox(width: 8.0),
-            Text('MyChef'),
-          ],
-        ),
-      ),
       body: CustomScrollView(
-        slivers: [],
+        slivers: [
+          SliverAppBar(
+            floating: true,
+            centerTitle: true,
+            actions: [
+              IconButton(icon: Icon(Icons.more_vert_outlined), onPressed: () {})
+            ],
+            title: Row(
+              children: [
+                Icon(
+                  Icons.restaurant_menu,
+                ),
+                SizedBox(width: 8.0),
+                Text('MyChef'),
+              ],
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              height: MediaQuery.of(context).size.height * 2,
+              color: Colors.red,
+            ),
+          ),
+        ],
       ),
     );
   }
